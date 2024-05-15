37f70134ae1afa6fb2b0dd1541aaf3122b360ebe
rlitoncs <rj_liton@hotmail.com>
2024-05-15 00:00:27 -0400

    Refactor code into separate functions and modules

--- play.js
+++ play.js
@@ -1,26 +1,2 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-
-  //connection setup
-  const config = {
-    host: 'localhost',
-    port: 50541,
-  };
-
-  //Create connection to server
-  const conn = net.createConnection(config);
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on("data", (serverData) => {
-    console.log(`Server says: ${serverData}`);
-  });
-  
-  return conn;
-};
-
-console.log("Connecting ...");
+const connect = require("./client");
 connect();
