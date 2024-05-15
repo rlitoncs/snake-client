86cb830e217287aad1af4f2baec9675dfdba974c
rlitoncs <rj_liton@hotmail.com>
2024-05-14 23:17:09 -0400

    Connect to server and Handle incoming Server data

---
+++ play.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+
+  //connection setup
+  const config = {
+    host: 'localhost',
+    port: 50541,
+  }
+
+  //Create connection to server
+  const conn = net.createConnection({config});
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on("data", (serverData) => {
+    console.log(`Server says: ${serverData}`);
+  })
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
